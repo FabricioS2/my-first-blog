de24e6dc846b03986e771dbad650ae84c17f37ff
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2023-07-27 23:25:41 -0300

    templates dinamicos

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = post.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_criacao') 
+    return render(request, 'blog/post_list.html', {'posts':posts})
