91087b2902f9404797f2d6922df0f4253a5a8833
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2023-07-28 01:51:07 -0300

    css e fim do site

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
 def post_list(request):
     posts = post.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_criacao') 
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail(request, pk):
+    posts = get_object_or_404(post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': posts})
